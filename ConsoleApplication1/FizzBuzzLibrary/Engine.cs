b9e51b8d70b89564b3f61998d1fa16a19cad24da
senseilee <reidkhlee@gmail.com>
2015-03-27 20:06:51 -0500

    Added some comments to the interface and engine.

--- ConsoleApplication1/FizzBuzzLibrary/Engine.cs
+++ ConsoleApplication1/FizzBuzzLibrary/Engine.cs
@@ -6,15 +6,27 @@ using System.Threading.Tasks;
 
 namespace FizzBuzzLibrary
 {
+    /// <summary>
+    /// Engine provides divisible algorithm for determining if rules are met.
+    /// </summary>
     public class Engine
     {
+        // Rules to be iterated over for client-specified number
         private readonly IEnumerable<IProvideFizzBuzzRule> rules;
 
+        /// <summary>
+        /// Constructor that requires a list of rules to be used.
+        /// </summary>
+        /// <param name="customRules">List of custom rules that satisfy
+        /// the IProvideFizzBuzzRule interface</param>
         public Engine(IEnumerable<IProvideFizzBuzzRule> customRules)
         {
             rules = customRules;
         }
 
+        /// <summary>
+        /// Debug method to quickly print results for numbers 1 through 100
+        /// </summary>
         public void PrintTo100()
         {
             for (int i = 1; i <= 100; ++i)
@@ -23,21 +35,34 @@ namespace FizzBuzzLibrary
             }
         }
 
-        public string Calculate(int i)
+        /// <summary>
+        /// Method to get string representing which rules were satisfied
+        /// </summary>
+        /// <param name="number">Number to test rules against</param>
+        /// <returns>Returns string containing concatenated words depending
+        /// if the rule was met.  If rule was not met, returns the number
+        /// passed in.</returns> 
+        public string Calculate(int number)
         {
             string matches = string.Empty;
 
+            // iterate through rules
             foreach (var rule in rules)
             {
-                if (i % rule.MultipleOf == 0)
+                // checks if number is divisible evenly by the
+                // rule's MultipleOf property
+                if (number % rule.MultipleOf == 0)
                 {
+                    // Evenly divisible, so concatenate WordMatch
                     matches += rule.WordMatch;
                 }
             }
 
             if (matches == string.Empty)
             {
-                matches = i.ToString();
+                // if matches is empty, then number was not divisible evenly.
+                // Set it number.
+                matches = number.ToString();
             }
 
             return matches;
--- ConsoleApplication1/FizzBuzzLibrary/IProvideFizzBuzzRule.cs
+++ ConsoleApplication1/FizzBuzzLibrary/IProvideFizzBuzzRule.cs
@@ -6,9 +6,20 @@ using System.Threading.Tasks;
 
 namespace FizzBuzzLibrary
 {
+    /// <summary>Interface that clients implement to define rules
+    /// for the FizzBuzz engine.
+    /// </summary> 
     public interface IProvideFizzBuzzRule
     {
+        /// <summary>Provides the word to be returned when a number
+        /// is a multiple of the MultipleOf property.
+        /// </summary> 
         string WordMatch { get; }
+
+        /// <summary>Defines value that if engine encounters a number
+        /// that is divisible by MultipleOf, it is considered a match
+        /// and the WordMatch is returned by the engine.
+        /// </summary> 
         int MultipleOf { get; }
     }
 }
